0de137ace9b512cb9342fc938068bcf9c1a7bbb7
jim <jimc@bethel.com.tw>
2021-05-25 08:26:45 +0800

    2021-05-25

--- gym/menu.js
+++ gym/menu.js
@@ -28,15 +28,49 @@ Vue.component('gym-menu', {
           {title: "上臂屈曲", start: 95, end: 103},
         ]
       }, {
-        title: "平板", id: "66gO0V2M-7w", 
+        title: "棒式運動-黑面蔡媽媽", id: "66gO0V2M-7w", 
         children: [
-          {title: "第一動", start: 5, end: 13},
-        
+          {title: "棒式基本型", start: 27.5, end: 70},
+          {title: "棒式後抬腿", start: 70, end: 105},
+          {title: "棒式開合跳", start: 105, end: 131},
+          {title: "棒式抬臀", start: 131.5, end: 160.8},
+          {title: "棒式扭臀", start: 166, end: 196.8},
         ]
+      }, {
+        title: "瘦小腹運動-黑面蔡媽媽", id: "6EDuWA56UwM", 
+        children: [
+          {title: "船式", start: 102, end: 131},
+          {title: "踩腳踏車", start: 132, end: 172},
+          {title: "俄羅斯旋轉", start: 182, end: 212},
+          {title: "仰臥抬腿", start: 221, end: 251},
+          {title: "仰臥交叉抬腿", start: 261.5, end: 291},
+          {title: "交叉卷腹觸腿", start: 301.7, end: 331},
+          {title: "卷腹", start: 342, end: 371.5},
+          {title: "觸足屈腹", start: 382, end: 411.5},
+          {title: "屈腿提臀", start: 422, end: 451.5},
+          {title: "橋式+蚌殻式", start: 461.5, end: 491.5},
+        ]
+      }, {
+        title: "彈力帶-黑面蔡媽媽", id: "u56LGvX3zdQ", 
+        children: [
+          {title: "站姿前推", start: 87.5, end: 133},
+          {title: "交替彎舉", start: 135.8, end: 179},
+          {title: "踩腳踏車", start: 188.8, end: 224},
+          {title: "跪姿後抬腿", start: 228.5, end: 271},
+          {title: "腿外展", start: 274.5, end: 303},
+        ]
+        // 
       }]
 		};
 	},
 	created(){
+    /*
+      }, {
+        title: "", id: "", 
+        children: [
+          {title: "", start: , end: },
+        ]
+    */
 	},
 	async mounted () {
 	
