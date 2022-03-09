5c87cb0b5bc31afe1e973af78c0ea61aeece2c31
jim <jimc@bethel.com.tw>
2022-03-09 08:04:02 +0800

    改名

---
+++ MyTube/list.js
@@ -0,0 +1,404 @@
+Vue.component('dlg-list', { 
+	template:  `<modal v-model="visible" id="list" :footer-hide="true" :width="width" :closable="false"" 
+			:draggable="true" :mask-closable="false" 
+			style=""			
+		>
+		<div slot="header" 
+				style="display: flex; flex-direction: row; align-items: center; padding: 10px 12px; background-color: rgb(70, 160, 240)">
+			<div style="flex: 1; color: white; font-size: 20px;">{{"清單：" + (typeof editdata.id == "string" && editdata.id.length > 0 ? editdata.id : "" )}}</div>
+			<Icon type="md-add" size="22" @click.native="addRow" 
+				v-if="cursor == -1"
+				style="cursor: pointer; color: white; margin-right: 10px;" />
+			<Icon type="md-checkmark" size="22" 
+				v-if="cursor == -1"
+				@click.native="close" style="cursor: pointer; color: white;" />
+		</div>
+		<div :style="{height: height + 'px', overflow: 'hidden'}">
+			<div style="height: 100%; overflow-y: auto; " id="listFrame">
+				<div style="flex: 1; padding: 5px;">
+					<i-input element-id="editTitle" size="large" v-model="title" style="flex: 1;" placeholder="title" />
+				</div>
+				<div style="flex: 1; padding: 0px 5px 5px 5px;">
+					<i-input v-if="typeof editdata.id == 'string' &&  editdata.id.length == 0" size="large"
+						element-id="editID" v-model="id" style="flex: 1; "  placeholder="id"/>
+				</div>
+
+				<div v-for="(item, index) in rows" :key="index" 
+					style="display: flex; flex-direction: row; justify-content: center; align-items: center;
+						padding: 5px 5px; min-height: 44px;	
+					"
+					class="list"
+				>
+					<div v-if="cursor == index" style="" :class="display">
+						<div>
+							<i-input v-if="display == 'col3'" size="large" element-id="editName" v-model="name" style="flex: 1; style='height: 100%;' " />
+							<div v-else>{{index + 1}}</div>
+						</div>
+						<div>
+							<i-input size="large" element-id="editStart" v-model="start" style="flex: 1; style='height: 100%;' " />
+						</div>
+						<div>
+							<i-input size="large" element-id="editEnd" v-model="end" style="flex: 1; style='height: 100%;' " />
+						</div>
+					</div>
+					<div v-else style="" @click="play(index); cursor = -1; " :class="display">
+						<div v-if="display == 'col3'">{{item.title}}</div>
+						<div v-else>{{index + 1}}</div>
+						<div>{{item.start}}</div>
+						<div>{{item.end}}</div>
+					</div>
+					<Icon v-if="cursor == index" type="md-headset" size="18" 
+						@click.native="play(index);" 
+						style="cursor: pointer; margin-left: 6px;" />
+					<Icon v-else type="md-create" size="18" 
+						@click.native="cursor = index; name = item.title; start = item.start; end = item.end" 
+						style="cursor: pointer; margin-left: 6px;" />
+					<Icon :type="cursor == index ? 'md-checkmark' : 'md-trash'" size="18"
+						@click.native="del(index)" 
+						style="cursor: pointer; margin-left: 6px;" />
+				</div>
+			</div>
+		</div>
+	</modal>`,
+	props: {
+		visible: {
+			type: Boolean,
+			default: false
+		},
+		display: {
+			type: String,
+			default: "col2" // col3
+		} ,
+		editdata: {
+			type: Object,
+			default() {
+				return {
+					id: "",
+					title: "",
+					children: []
+				}
+			}
+		} ,
+	},
+	data() {
+		return {
+      title: "", 
+      id: "",
+			rows: [],
+			cursor: -1,
+			name: "",
+			start: "",
+			end: "",
+			width: 0,
+			height: 0,
+			dirty: false,
+			mode: "new"
+		};
+	},
+	created(){
+	},
+	async mounted () {
+		// console.log(this.editdata)
+		this.width =  350;
+		this.height = 400;
+		let el = document.querySelector("#list .ivu-modal");
+		el.style.margin = "0px";
+		setTimeout(()=>{
+			el = document.querySelector("#list .ivu-modal-content-drag");
+			el.style.left = (document.body.clientWidth - (this.width + 20) + 
+				(15)) + "px";
+			el.style.top = (document.body.clientHeight - (this.height + 150)) + "px";
+			// console.log("clientHeight: " + document.body.clientHeight)
+		}, 600)
+		this.broadcast.$on('onResize', this.onResize);
+	},
+	destroyed() {
+		this.broadcast.$off('onResize', this.onResize);
+  },
+	methods: {
+		isNumber(s) {
+			if(("" + s).length == 0) return false;
+			let exp = /[0-9]/;
+			let arr = ("" + s).split(".");
+			for(let x = 0; x < arr.length; x++) {
+				let str = arr[x];
+				for(let i = 0; i < str.length; i++) {
+					let chr = str.substr(i, 1);
+					let b = exp.test(chr);
+					if(b == false) {
+						return false;
+					}
+				}
+			}
+			return true;
+		},
+		onKeydown(event) {
+			let o = document.activeElement;
+			let pk = navigator.userAgent.indexOf('Macintosh') > -1 ? event.metaKey : event.ctrlKey;
+			let ck = navigator.userAgent.indexOf('Macintosh') > -1  ? event.ctrlKey : event.altKey;
+			let ok = navigator.userAgent.indexOf('Macintosh') > -1  ? event.altKey : false;
+			let sk = event.shiftKey, code = event.keyCode;
+			let id = event.target.id;
+			// console.log("key: " + code + ", pk: " + pk + ", cntrl: " + ck + ", option: " + ok);
+
+			// console.log(String.fromCharCode(code) + ": " + (/\d/.test(String.fromCharCode(code))))
+			if(o.tagName == "INPUT" && this.visible == true){
+				let b = true;
+				if(pk && code == 82) {
+					b = false;
+				} else if(code == 13) { // enter
+					if(id == "editName") {
+						if(o.value.trim().length > 0) {
+							if(o.value.trim() != this.rows[this.cursor].title) {
+								this.rows[this.cursor].title = o.value.trim();
+								this.dirty = true;
+							}
+							document.getElementById("editStart").focus();
+						}
+					} else if(id == "editStart" || id == "editEnd") {
+						let editStart = document.getElementById("editStart").value.trim();
+						if(editStart.length > 0 && this.isNumber(editStart)) {
+							if(this.rows[this.cursor].start != parseFloat(editStart)) {
+								this.rows[this.cursor].start = parseFloat(editStart);
+								this.dirty = true;
+							}
+						}
+						let editEnd = document.getElementById("editEnd").value.trim();
+						if(editEnd.length > 0 && this.isNumber(editEnd)) {
+							if(this.rows[this.cursor].end != parseFloat(editEnd)) {
+								this.rows[this.cursor].end = parseFloat(editEnd);
+								this.dirty = true;
+							}
+						}
+						if(o.value.trim().length == 0 || !this.isNumber(o.value.trim()))
+							return false;
+						else if(id == "editStart" )
+							document.getElementById("editEnd").focus();
+						else
+							this.reset();
+					}
+				} else if((id == "editStart" || id == "editEnd") && ok && (code == 37 || code == 39)) {
+					if(!this.isNumber(document.getElementById(id).value)) return;
+
+					let x = (code == 37 ? -0.5 : 0.5) + toNumber(document.getElementById(id).value);
+					let start = 0, end = 0;
+					if(id == "editStart") {
+						this.start = x;
+						start = x;
+						end = x + 5;
+					} 						
+					else { 
+						start = this.isNumber(document.getElementById("editStart").value) 
+							? toNumber(document.getElementById("editStart").value) : x - 3;
+						console.log(start)
+						end = x;
+						this.end = x;
+						start = end - start > 3 ? end - 3 : start;
+					}
+					// console.log("start: " + start + ", end: " + end)
+					this.$emit('on-click-play', {end: end, start: start});
+				} else if((id == "editStart" || id == "editEnd") && ok && code == 80) { // p
+					if(!this.isNumber(document.getElementById(id).value)) return;
+					let x = toNumber(document.getElementById(id).value);
+					// console.log(x + ", " + typeof x)
+					let start = 0, end = 0;
+					if(id == "editStart"){
+						start = x;
+						end = x + 5;
+					} else {
+						start = this.isNumber(document.getElementById("editStart").value) 
+							? toNumber(document.getElementById("editStart").value) : x - 3;
+						end = x;
+						start = end - start > 3 ? end - 3 : start;
+					}
+					// console.log(start + "; " + end)
+					this.$emit('on-click-play', {end: end, start: start});
+				} else if((id == "editStart" || id == "editEnd") && pk) {
+					// document.getElementById(id).value = player.getCurrentTime().toFixed(2);
+					if(id == "editStart") {
+						let x = player.getCurrentTime() + 0.2;
+						this.start = x.toFixed(2);
+						this.$emit('on-click-play', {end: x + 5, start: x});
+					} else 
+						this.end = player.getCurrentTime().toFixed(2)
+				} else if(code == 27) {
+					this.reset();
+				// } else if ((id == "editStart" || id == "editEnd") && code > 47 && code < 58) {
+				// 	e.preventDefault();
+				// 	return false;
+				} else {
+					b = false;
+				}
+				if(b == true) {
+					event.preventDefault();
+					event.stopImmediatePropagation();
+					event.stopPropagation();				
+				}
+			}
+
+			function toNumber(num) {
+				return parseFloat(parseFloat(num).toFixed(2));
+			}
+		},
+		reset(){
+			this.name = ""; this.start = ""; this.end = "";
+			this.cursor = -1;
+		},
+		addRow(){
+			this.rows.push({})
+			this.cursor = this.rows.length - 1;
+			this.name = ""; this.start = ""; this.end = "";
+		},
+		close(){
+			let bTitle = this.title.trim().length > 0 && this.title != this.editdata.title;
+			let bID = this.id.trim().length > 0 && this.id != this.editdata.id;
+			let children = [];
+			if(this.dirty == true) {
+				this.rows.forEach(item=>{
+					let bChange = (this.display == "col3" && item.title.trim().length > 0) || this.display == "col2" 
+						? true : false;
+					if(bChange == true && ("" + item.start).trim().length > 0 && ("" + item.end).trim().length > 0) {
+						let json = {
+							
+							start: parseFloat(item.start), 
+							end: parseFloat(item.end)
+						}
+						if(this.display == "col3") {
+							json.title = item.title;
+						}
+						children.push(json)
+					}
+				})
+			} else if(bTitle == true || bID == true) {
+				children = this.rows;
+			}
+			if(bTitle == true || bID == true || children.length > 0) {
+				let json = Object.assign(this.editdata, {title: this.title, id: this.id, children})
+				this.$emit("update", json);
+			}
+			this.reset();
+			this.$emit("close");
+		},
+		del(index){
+			if(this.cursor == -1) {
+				this.rows.splice(index, 1);
+				this.dirty = true;
+				this.reset();
+			} else {
+				let name = this.name, start = this.start, end = this.end;
+				let cursor = this.cursor;
+				this.reset();
+
+				if(((this.display == "col3" && name.trim().length > 0) || this.display == "col2") && this.isNumber(start) && this.isNumber(end)) {
+					let bChange = this.display == "col3" && (this.rows[cursor].title == name) ? false : true;
+					if(bChange == false && this.rows[cursor].start == start && this.rows[cursor].end == end) return;
+
+					if(parseFloat(start) >= parseFloat(end)) return;
+					if(this.display == "col3") this.rows[cursor].title = name;
+					this.rows[cursor].start = parseFloat(start); 
+					this.rows[cursor].end = parseFloat(end);
+					this.dirty = true;
+				}
+			}
+		},
+		play(index){
+			let start = 0, end = 0;
+			if(this.cursor == index) {
+				start = document.getElementById("editStart").value;
+				end = document.getElementById("editEnd").value;
+			} else {
+				start = this.rows[index].start;
+				end = this.rows[index].end;
+			}
+			// console.log({end, start})
+			this.$emit('on-click-play', {end, start});
+		},
+		onResize(){
+			clearTimeout(this.resizeId);
+			this.resizeId = setTimeout(()=>{
+				let el = document.querySelector("#list .ivu-modal-content-drag");
+				if(el == null) 
+					return;
+				let left = el.style.left.replace("px", "");
+				if(left > document.body.clientWidth - (this.width + 20))
+					el.style.left = (document.body.clientWidth - (this.width + 20)) + "px";
+				let top = el.style.top.replace("px", "");
+				if(top > document.body.clientHeight - (this.height + 150))
+					el.style.top = (document.body.clientHeight - (this.height + 150)) + "px";
+			}, 300);
+		},
+		onFocus(){
+			// if(this.cursor >= 0) {
+			// 	setTimeout(() => {
+			// 		this.cursor = -1; 
+			// 		this.name = "";
+			// 	}, 600);
+			// }
+		},
+		onChange(e) {
+			this.dirty = true;
+		}
+	},
+	computed: {	
+	},
+	watch: {
+		editdata(value) {
+			this.mode = value.id.length == 0 ? "new" : "edit";
+			this.title = value.title;
+			this.id = value.id;
+			this.rows = [];
+			if(Array.isArray(value.children)){
+				value.children.forEach(el => {
+					this.rows.push(Object.assign({}, el))
+				});
+			}
+			this.dirty = false;
+			setTimeout(() => {
+				let el = document.getElementById("editTitle");
+				el.focus();
+			}, 700);
+		},
+		cursor(value) {
+			if(value > -1) {
+				setTimeout(() => {
+					let el;
+					if(this.display == "col3") {
+						el = document.getElementById("editName");
+						el.focus();
+						el.addEventListener('keydown', this.onKeydown, false);
+						el.addEventListener('blur', this.onFocus, false);
+						el.addEventListener('change', this.onChange, false);
+					}
+
+					el = document.getElementById("editStart");
+					el.addEventListener('keydown', this.onKeydown, false);
+					el.addEventListener('blur', this.onFocus, false);
+					el.addEventListener('change', this.onChange, false);
+					if(this.display == "col2") el.focus();
+
+					el = document.getElementById("editEnd");
+					el.addEventListener('keydown', this.onKeydown, false);
+					el.addEventListener('blur', this.onFocus, false);
+					el.addEventListener('change', this.onChange, false);
+				}, 300);
+			} else {
+				let el;
+				if(this.display == "col3") {
+					el = document.getElementById("editName");
+					el.removeEventListener('keydown', this.onKeydown, false);
+					el.removeEventListener('keydown', this.onFocus, false);
+					el.removeEventListener('change', this.onChange, false);
+				}
+				el = document.getElementById("editStart");
+				el.removeEventListener('keydown', this.onKeydown, false);
+				el.removeEventListener('keydown', this.onFocus, false);
+				el.removeEventListener('change', this.onChange, false);
+
+				el = document.getElementById("editEnd");
+				el.removeEventListener('keydown', this.onKeydown, false);
+				el.removeEventListener('keydown', this.onFocus, false);
+				el.removeEventListener('change', this.onChange, false);
+			}
+		}
+	}
+});
--- MyTube/player.js
+++ MyTube/player.js
@@ -1,14 +1,40 @@
-Vue.component('my-player', { 
+
+let idPlay;
+
+Vue.component('yt-player', { 
 	template:  `
-    <div style="padding: 2px 2px 15px 2px;" id="btnPlays">
-      <i-button v-for="(item, index) in rows" :key="index"
-				:type="active == index || prev == index ? 'warning' : 'default'"
-				:ghost="prev == index"
-        @click.native="onClick(index)">
-        {{item.title}}
-      </i-button>
-			<a id="link" href="" target="_blank">下載 MP3</a>
-    </div>
+		<div style="display: flex; flex-direction: row; justify-content: flex-start; align-items: flex-start; ">
+			<div id="btnPlays"
+				style="padding: 2px; flex: 1;"
+				:style="{height: (rows.length == 0 && $isDebug() && $isLogin() ? '60px' : 
+				smallScreen ? '120px' : 'auto')}"
+			>
+				<i-button v-for="(item, index) in rows" :key="index" class="btn"
+					:type="active == index || prev == index ? 'warning' : 'default'"
+					:ghost="prev == index"
+					@click.native="onClickPlay(index)">
+					<span v-if="typeof item.title == 'string' ">{{item.title}}</span>
+					<span v-else>{{(index + 1)}}</span>
+				</i-button>
+			</div>
+			<div style="margin: 3px 10px 3px 0px;">
+				<div v-if="$isDebug() && $isLogin() && videoId.length > 0"
+					style="flex-direction: row; margin-bottom: 5px;"
+				>
+					<i-button type="success"  
+						@click.native="onClickList()" icon="md-create" shape="circle" />
+
+					<i-button type="success"
+						@click.native="onClickEdit()" icon="md-document" shape="circle"
+					/>
+				</div>
+				<i-button :type="isPlaying ? 'default' : 'primary'" 
+					:icon="isPlaying ? 'md-pause' : 'md-walk'"
+					v-if="rows.length > 30 && $isLogin() && videoId.length > 0"
+					@click.native="countdown()"  shape="circle"
+				/>
+			</div>
+		</div>
   `,
 	props: {
 		// title: String
@@ -18,54 +44,148 @@ Vue.component('my-player', {
       rows: [],
 			active: -1,
 			prev: -1,
-			videoId: ""
+			videoId: "",
+			content: undefined,
+			height: 0,
+			isPlaying: false,
+			smallScreen: true, 
+			cycle: 0
 		};
 	},
 	created(){
 	},
 	mounted () {
     this.broadcast.$on('onPlayerReady', this.onPlayerReady);
-		console.log("debug: " + this.$isDebug())
+		window.addEventListener('keydown', this.onKeydown, false);
+		this.broadcast.$on('onResize', this.onResize);
 	},
 	destroyed() {
+		window.removeEventListener('keydown', this.onKeydown, false);
     this.broadcast.$off('onPlayerReady', this.onPlayerReady);
-  },
-	destroyed() {		
+		this.broadcast.$off('onResize', this.onResize);
+		this.stop();
   },
 	methods: {
-    async play(item){
-			console.log(item.key)
-			let link = document.getElementById("link");
-			link.href = `https://www.yout.com/watch?v=${item.key}`;
-			console.log(`https://www.youtube.com/watch?v=${item.key}`)
-
+		onResize(small){
+			this.smallScreen = small;
+		},
+		async play(item){
+			this.content = item;
+			this.prev = -1;
 			this.active = -1;
       this.rows = Array.isArray(item.children) ? item.children : [];
-			// this.rows = [{title: 1, start: 20, end: 25}];
 			this.videoId = item.id;
-			let m = window.localStorage["mytube-" + item.key];
-			if(typeof m == "string") {
-				let x = this.rows.findIndex((item)=>{
-					return item.title == m;
-				});
-				if(x > -1) this.prev = x;
+			let m = window.localStorage["yt-" + this.videoId];
+			this.stop();
+
+			let x = -1;
+			if(typeof m != "undefined") {
+				if(isNaN(m)) {
+					x = this.rows.findIndex((item)=>{
+						return item.title == m;
+					});
+				} else {
+					x = parseInt(m);
+				}
+				if(x > -1) 
+					this.prev = x;
+				else 
+					this.prev = 0;
+			} else if(this.rows.length > 0) {
+				this.prev = 0;
 			}
 			let el = document.getElementById("btnPlays");
 			el.style.visibility = "hidden";
+			if(this.prev > -1) {
+				setTimeout(() => {
+					let arr = document.querySelectorAll(".btn")
+					arr[this.prev].focus();
+				}, 600);				
+			}
     },
+		stop(){
+			if(player) player.pauseVideo();
+			this.isPlaying = false;
+			clearTimeout(idPlay)
+			this.broadcast.$off('playend', this.playend);
+		},
 		onPlayerReady(){
 			setTimeout(() => {
 				let el = document.getElementById("btnPlays");
-				el.style.visibility = "visible";				
-			}, 1000);
+				el.style.visibility = "visible";
+			}, 300);
 		},
-    onClick(index) {
-      this.$emit('on-click', this.rows[index]);
+    onClickPlay(index) {
+      this.$emit('on-click-play', this.rows[index]);
 			this.active = index;
 			this.prev = -1;
-			window.localStorage["mytube-" + this.videoId] = this.rows[index].title;
-			console.log(index)
-    }
+			if(this.isPlaying == false)
+				window.localStorage["yt-" + this.videoId] = typeof this.rows[index].title == 'string' ? this.rows[index].title : index;
+			else {
+				// console.log("onClickPlay: " + index + ", " + (new Date()))
+			}
+			if(index > -1 && index < this.rows.length) {
+				this.broadcast.$emit('exam', index);
+				setTimeout(() => {
+					let arr = document.querySelectorAll(".btn")
+					arr[index].focus();
+				}, 600);				
+			}
+    },
+		onKeydown(event){
+			let o = document.activeElement;
+			// let pk = navigator.userAgent.indexOf('Macintosh') > -1 ? event.metaKey : event.ctrlKey;
+			// let ak = navigator.userAgent.indexOf('Macintosh') > -1  ? event.ctrlKey : event.altKey;
+			// let sk = event.shiftKey, code = event.keyCode;
+			// let char = (event.keyCode >=48 && event.keyCode <=122) ? String.fromCharCode(event.keyCode).toUpperCase() : "";
+			// console.log(event.keyCode + ", " + this.active)
+			// console.log(o.id)
+			if(this.rows.length == 0 || this.$isDialog() == true) return;
+			if(o.tagName == "INPUT") return;
+			if(event.keyCode == 32) {
+				if(this.prev > -1)
+					this.onClickPlay(this.prev)
+				else if(this.active > -1)
+					this.onClickPlay(this.active);
+			} else if(event.keyCode == 37 || event.keyCode == 38) { // left, up
+				if(this.active > 0) {
+					this.onClickPlay(this.active - 1)
+				}
+			} else if(event.keyCode == 39 || event.keyCode == 40) { // right, right
+				if(this.active < this.rows.length - 1) {
+					this.onClickPlay(this.active + 1)
+				}
+			}
+		},
+		onClickList(){
+			// this.stop();
+			this.$emit('on-click-list');
+		},
+		onClickEdit(){
+			// this.stop();
+			this.$emit('on-click-edit');
+		},
+		countdown(index){
+			if(this.isPlaying == true) {
+				this.stop();
+			} else {
+				clearTimeout(idPlay)
+				this.isPlaying = true;
+				this.broadcast.$on('playend', this.playend);
+				this.cycle = 0;
+				this.onClickPlay(this.cycle)
+			}
+		},
+		playend(){
+			if(this.cycle < this.rows.length - 1 && this.isPlaying == true) {
+				this.cycle++;
+				idPlay = setTimeout(() => {
+					this.onClickPlay(this.cycle);
+				}, 1000 * 10);
+			} else {
+				this.stop();
+			}
+		}
 	},
 	computed: {
 	},
