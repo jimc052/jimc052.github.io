5c6551285035aed996105cc18e2e907c1e912629
jim <jimc@bethel.com.tw>
2022-08-19 09:07:59 +0800

    2022-08-19

--- shiftTime/calendar.js
+++ shiftTime/calendar.js
@@ -1,30 +1,3 @@
-/*
-<!-- 非當月 -->
-					<div v-if="day.available == false" style="height: 100%; display: flex; flex-direction: column;">
-						<div style="text-align: center; color: #D3D3D3;">{{day.day}}</div>
-						<div v-if="typeof day.holiday == 'string'" 
-							style="text-align: center; color: rgba(0,0,0, 0.3);  background-color: rgba(0, 255, 0, 0.2);">
-							{{day.holiday}}
-						</div>
-            <div style="text-align: center;">{{day.shift}}</div>
-					</div>
-          <div v-else style="height: 100%; display: flex; flex-direction: column;" v-on:click="onClickCell(index1, index2, day)">
-            <!-- 國定假日 -->
-            <div v-if="typeof day.holiday == 'string'" style="flex: 1;">
-              <div style="text-align: center;">{{day.day}}</div>
-              <div style="text-align: center; background-color: green; color: white;">
-                {{day.holiday}}
-              </div>
-            </div>
-            <!-- 周末，周日 -->
-            <div v-else-if="index2 >= 5" style="flex: 1; text-align: center; color: #FF8C00;">
-              {{day.day}}
-            </div>
-            <div v-else style="flex: 1; text-align: center;">{{day.day}}</div>
-
-            <div style="text-align: center;">{{day.shift}}</div>
-          </div>
-*/
 Vue.component('calendar', {
 	template:  `<div id="calendar" style="display: flex; flex-direction: column; height: 100%; width: 100%;">
 		<div :style="{background: '#2d8cf0', 
@@ -38,8 +11,10 @@ Vue.component('calendar', {
 			<Icon type="ios-arrow-forward" size="28" @click.native="onClickIcon(1)" 
 			style="cursor: pointer; margin-left: 10px;" />
 			<div style="flex: 1; text-align: right;">
+				<!--
 				<Icon type="md-more" size="28" @click.native="onClickIconMore" 
 				style="cursor: pointer; margin-right: 10px;" />
+				-->
 			</div>
 		</div>
 
@@ -55,31 +30,38 @@ Vue.component('calendar', {
 			<div class="row" v-for="(weeks, index1) in schedule" style="flex: 1; display: flex; flex-direction: row;">
 				<div class="col" v-for="(day, index2) in weeks"
 					v-on:click="onClickCell(index1, index2, day)"
-
 					:style="{flex: '1', height: '100%', padding: '5px', 
 						backgroundColor: day.available ? (day.workday == 2 ? 'rgba(45, 140, 250, 0.2)' : 'white') : '#F8F8F8',
-					}">
+						cursor: day.available && day.workday >= 2 && edit == true ? 'pointer' : 'default'
+					}"
+				>
 
-					<div class="date" :class="{inavailable: day.available == false, weekend: day.available == true && index2 >= 5}">
+					<div class="date text" 
+						:class="{inavailable: day.workday < 2, weekend: day.available == true && index2 >= 5}">
             {{day.day}}
           </div>
 					<div v-if="typeof day.holiday == 'string'" 
-						style="text-align: center; " :class="{holiday0: !day.available, holiday1: day.available,}">
+						class="text holiday" :class="{inavailable: day.workday < 2}"
+					>
 						{{day.holiday}}
 					</div>
 
 					<div style="flex: 1" />
 
-					<div >
-						{{day.workday}}
+					<div class="text shift" :class="{inavailable: day.workday < 2, am: day.shift == '早班', pm: day.shift == '晚班', break: day.shift == '休假' }">
+						{{day.shift}}
 					</div>
+					
 				</div>
 			</div>
 		</div>
 
-    <div style="display: flex; flex-direction: row; justify-content: center; align-items: center; padding: 10px;" >
+    <div style="display: flex; flex-direction: row; justify-content: center; align-items: center; padding: 10px;" 
+			v-if="month.toString('yyyymm') >= today.toString('yyyymm')"
+		>
 			<div style="flex: 1" />
-			<RadioGroup v-model="mode" type="button" button-style="solid" size="large" @on-change="onRadioChange">
+
+			<RadioGroup v-if="edit == true" v-model="shift" type="button" button-style="solid" size="large" @on-change="onRadioChange">
 					<Radio label="早班"></Radio>
 					<Radio label="晚班"></Radio>
 					<Radio label="休假"></Radio>
@@ -88,14 +70,12 @@ Vue.component('calendar', {
 			
 			<div style="flex: 1" />
 
-			<Icon type="md-checkmark" size="28" @click.native="onClickIconMore" 
-				style="cursor: pointer; margin-right: 10px;" />
-			<Icon type="md-close" size="28" @click.native="onClickIconMore" 
-				style="cursor: pointer; margin-right: 10px;" />
-			<Icon type="md-cloud-upload" size="28" @click.native="onClickIconMore" 
-				style="cursor: pointer; margin-right: 10px;" />
-			<Icon type="md-create" size="28" @click.native="onClickIconMore" 
-			style="cursor: pointer; margin-right: 10px;" />
+			<Button v-if="edit == false" size="large" icon="md-create" type="primary" @click="onClickIconEdit" />
+
+			<Button v-if="$isLogin() && dirty == true && edit == true" size="large" style="margin-right: 3px;"
+				icon="md-cloud-upload" type="success" @click="onClickIconSave" />
+
+			<Button v-if="edit == true" size="large" icon="md-close" type="primary" @click="onClickIconClose" />			
     </div>
 	</div>`,
 	props: {
@@ -108,7 +88,9 @@ Vue.component('calendar', {
 			today: new Date(),
 			month: new Date(),
 			visible: false, 
-      mode: "早班"
+      shift: "早班", 
+			edit: false,
+			dirty: false
 		};
 	},
 	created(){	
@@ -116,19 +98,64 @@ Vue.component('calendar', {
 	},
 	async mounted () {
 		this.retrieve();
+		this.onClickIconSave()
 	},
 	destroyed() {
   },
 	methods: {
 		onRadioChange(value) {
-			console.log(value)
+			// console.log(value, this.shift)
 		},
 		onClickCell(x, y, data){
-			console.log(x, y, JSON.stringify(data));
+			if(this.edit == true && data.available && data.workday >= 2 && data.shift != this.shift) {  // 
+				let arr = this.schedule[x];
+				data.shift = (this.shift == "無") ? undefined : this.shift;
+				arr[y] = data;
+				this.$set(this.schedule, x, arr);
+				
+				let json = {};
+				for(let i = 0; i < this.schedule.length; i++) {
+					for(let j = 0; j < this.schedule[i].length; j++) {
+						let row = this.schedule[i][j];
+						if(typeof row.shift == "string") {
+							json["" + row.day] = row.shift;
+						}
+					}
+				}
+				this.$storage("shiftTime=" + this.month.toString("yyyy-mm"), json);
+				console.log(this.$storage("shiftTime=" + this.month.toString("yyyy-mm")))
+				this.dirty = true;
+			}
 			// this.$refs["setting"].visible = true;
 		},
-		onClickIconMore(){
-			// this.$refs["setting"].visible = true;
+		onClickIconEdit(){
+			this.edit = true;
+		},
+		onClickIconClose() {
+			this.dirty = false;
+			this.edit = false;
+		},
+		async onClickIconSave() {
+			let key = this.month.toString("yyyy-mm");
+			try {
+				let ref = FireStore.db.collection("users").doc(FireStore.uid())
+					.collection("ShiftTime").doc(key);
+
+				let myRecords = this.$storage("shiftTime=" + key)
+				if(typeof myRecords == "string" && myRecords.length > 0) {
+					myRecords = JSON.parse(myRecords)
+				} else {
+					myRecords = {};
+				}
+
+				let x = await ref.set(myRecords, {merge: true});
+				console.log(x)
+			} catch(e) {
+				console.log(e)
+			}
+
+			this.dirty = false;
+			this.edit = false;
 		},
 		onChangeSetting(value){
 			console.log("onChangeSetting: " + value)
@@ -167,6 +194,12 @@ Vue.component('calendar', {
 			let days = d1.getDay() * -1;
 			let myRecords = this.$storage("shiftTime=" + month);
 			let mySchedule = [];
+			console.log(myRecords)
+			if(typeof myRecords == "string" && myRecords.length > 0) {
+				myRecords = JSON.parse(myRecords)
+			} else {
+				myRecords = {};
+			}
 
 			d1.addDays(days + (days == 0 ? -6 : 1));
 			let today = this.today.toString("yyyy-mm-dd");
@@ -182,21 +215,23 @@ Vue.component('calendar', {
 						holiday: typeof holiday[s1] == "string" ? holiday[s1] : undefined,
 						workday: 0,
 					};
-					if((d1.getDay() > 0 && d1.getDay() < 6) || workday.indexOf(s1) > -1) {
+					// <!-- 工作日 workday： 1 今天以前，2 今天，3 今天以後 -->
+					if(s1.indexOf(month) > -1) {// (d1.getDay() > 0 && d1.getDay() < 6) || workday.indexOf(s1) > -1) {
 						obj.workday = s1 == today ? 2 : (s1 > today ? 3 : 1)
 					}
 					obj.editable = s1 >= this.today.toString("yyyy-mm-dd");
+					if(obj.available == true && typeof myRecords["" + obj.day] == "string") {
+						obj.shift = myRecords["" + obj.day];
+						delete myRecords["" + obj.day];
+					}
 					arr.push(obj);
 					d1.addDays(1);
 				}
-				mySchedule.push(arr)
+				mySchedule.push(arr);
 				if(d1.toString("yyyy-mm") > month) break;
 			}
-      console.log(mySchedule)
-			return {schedule: mySchedule, recorder: myRecords};
+			return {schedule: mySchedule};
 		},
-		onClickDay(day) {
-		}, 
 	},
 	watch: {
 		
