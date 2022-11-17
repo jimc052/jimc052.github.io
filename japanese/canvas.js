36c2ac8b4f813c2a8ea414dce59607d475c324ac
jim <jimc@bethel.com.tw>
2022-11-17 09:01:34 +0800

    2022-11-17

--- japanese/canvas.js
+++ japanese/canvas.js
@@ -51,8 +51,8 @@ Vue.component('vm-canvas', {
 			
 			let width = canvas.width, height = canvas.height;
 			if (window.devicePixelRatio) {
-				// canvas.style.width = width + "px";
-				// canvas.style.height = height + "px";
+				canvas.style.width = width + "px";
+				canvas.style.height = height + "px";
 				canvas.height = height * window.devicePixelRatio;
 				canvas.width = width * window.devicePixelRatio;
 				ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
