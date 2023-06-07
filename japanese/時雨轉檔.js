eea884edc96f350f32595d0f6414def86416c9a9
jim <jimc@bethel.com.tw>
2023-06-07 12:28:26 +0800

    2023-06-07

---
+++ japanese/時雨轉檔.js
@@ -0,0 +1,44 @@
+let h4s = document.querySelectorAll("h4");
+let divs = document.querySelectorAll("div");
+let tables = document.querySelectorAll("table");
+// console.log(h4s)
+// console.log(tables)
+let result = "";
+for(let i = 0; i < h4s.length; i++) {
+  console.log(i + ": " + h4s[i].innerText)
+  let 分類 = h4s[i].innerText == "綜合" || 分類 == "片假名" 
+    ? undefined : h4s[i].innerText;
+  let trs = tables[i].querySelectorAll("tr");
+  let colNames = trs[0].innerText.split("\t");
+  if(分類 == "片假名") 
+    colNames[0] = "假名";
+  
+  //日文,假名,重音,中文
+  // console.log("  => " + trs[0].innerText + ", " + typeof(trs[0].innerText) + ", " + Array.isArray(trs[0].innerText))
+  // if(i < 1) {
+    for(let j = 1; j < trs.length; j ++){
+      let json = {分類};
+      let tds = trs[j].innerText.split("\t");
+      for(let k = 0; k < colNames.length; k++) {
+        if("日文,假名,重音,中文".indexOf(colNames[k]) > -1)
+          json[colNames[k]] = tds[k];
+      }
+      result += (result.length > 0 ? ",\n" : "") + "  " + JSON.stringify(json);
+    }
+  // }
+
+  divs[i].style.display = "none";
+  h4s[i].style.display = "none";
+  tables[i].style.display = "none";
+}
+
+document.body.style.margin = "0px";
+document.body.style.padding = "0px";
+
+let textarea = document.createElement("textarea");
+textarea.style.width = "100%";
+textarea.style.height = "100%";
+textarea.style.fontSize = "16px"
+textarea.value = result;
+
+document.body.appendChild(textarea);
