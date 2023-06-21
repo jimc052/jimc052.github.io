0d57babbaee3be43868552b419a0d3ca9e1f5e8e
jim <jimc@bethel.com.tw>
2023-06-21 09:05:36 +0800

    2023-06-21

--- japanese/lesson.js
+++ japanese/lesson.js
@@ -1,12 +1,6 @@
 Vue.component('lesson', { 
 	template:  `<div style="height: 100%; width: 100%; overflow: auto; display: flex; flex-direction: column; padding: 10px;">
-		<div v-if="scrollTop <= 60" style="display: flex; flex-direction: row;">
-			<Select v-model="option"  size="large" @on-change="onChangeLesson"
-				style="width:150px; margin-bottom: 5px;"
-			>
-				<Option v-for="item in options" :value="item" :key="item">{{ item }}</Option>
-			</Select>
-			<div style="flex: 1;" ></div>
+		<div style="display: flex; flex-direction: row;">
 			<RadioGroup v-model="mode" type="button" button-style="solid" 
 				size="large"
 				@on-change="onChangeMode"
@@ -15,10 +9,19 @@ Vue.component('lesson', {
 				<Radio label="課文">課文</Radio>
 				<Radio label="單字">單字</Radio>
 			</RadioGroup>
+
+			<Select v-model="option"  size="large" @on-change="onChangeLesson"
+				style="margin-left: 20px; width:120px; margin-bottom: 5px;"
+			>
+				<Option v-for="item in options" :value="item" :key="item">{{ item }}</Option>
+			</Select>
+			<div style="flex: 1;" ></div>
+
 		</div>
 
-    <div style="flex: 1; padding: 10px; border: 1px solid rgb(220, 222, 226); overflow: auto;" v-html="html"
-			@scroll="onScroll"
+    <div ref="frame" class="lesson-frame" v-html="html" @scroll="onScroll"
+			style="background: white; flex: 1; padding: 10px; border: 1px solid rgb(220, 222, 226); overflow: auto;
+			"
 		></div>
 	</div>`,
 	props: {
@@ -29,50 +32,73 @@ Vue.component('lesson', {
       option: "",
 			html: "",
 			scrollTop: 0,
-			mode: "課文"
+			mode: "課文",
 		};
 	},
 	created(){
 	},
 	async mounted () {
-		TTX.initial();
-		// this.onResize();
-		// this.broadcast.$on('onResize', this.onResize);
-		window.addEventListener('keydown', this.onKeydown, false);
-		await this.$appendScript("./datas/大家的日本語/課文.js");
+		setTimeout(() => {
+			this.onResize();
+		}, 300);
+		this.broadcast.$on('onResize', this.onResize);
 
-		for(let key in 課文){
-			this.options.push(key)
-		}
+		TTX.initial();
+
+		window.addEventListener('keydown', this.onKeydown, false);
 
 		let s = window.localStorage["japanese-大家的日本語-lesson"];
 		if(typeof s == "string") {
 			this.option = s;
-		} else {
-			this.option = this.options[0];
 		}
-		if(this.option.length > 0)
-			this.onChangeLesson();
-
-		this.$refs["radio-group"].$children[0].currentValue = true;
+		s = window.localStorage["japanese-大家的日本語-mode"];
+		if(typeof s == "string") {
+			this.mode = s;
+		}
+		this.$refs["radio-group"].$children[this.mode == "課文" ? 0 : 1].currentValue = true;
+		this.onChangeMode();
 	},
 	destroyed() {
 		// this.broadcast.$off('onResize', this.onResize);
 		window.removeEventListener('keydown', this.onKeydown, false);
 		this.$removeScript("./datas/大家的日本語/課文.js");
+		this.$removeScript("./datas/大家的日本語/單字.js");
 		課文 = undefined;
+		單字 = undefined;
   },
 	methods: {
+		onResize(){
+			let frame = this.$refs["frame"];
+			if(typeof frame == "object") {
+				if((this.mode == "課文" && frame.clientWidth > 1000)) {
+					frame.classList.add("big-screen");
+					frame.classList.remove("small-screen");
+				} else if((this.mode == "單字" && frame.clientWidth > 800)) {
+						frame.classList.add("big-screen");
+						frame.classList.remove("small-screen");
+				} else {
+					frame.classList.add("small-screen");
+					frame.classList.remove("big-screen")
+				}
+			}
+			this.changeWidth();
+		},
 		onScroll(e) {
 			this.scrollTop = e.srcElement.scrollTop;
 		},
 		onKeydown() {
 		},
 		onChangeLesson() {
+			this.scrollTop = 0;
+			window.localStorage["japanese-大家的日本語-lesson"] = this.option;
+			this.html = "";
+			this.$refs["frame"].style.visibility = "hidden";
+
 			let parseLesson = async () => {
 				if(typeof 課文[this.option] == "object") {
+					//  style="margin-top: ${this.html.length > 0 ? 10 : 0}px;"
 					for(let key in 課文[this.option]) {
-						this.html += `<h3 style="margin-top: ${this.html.length > 0 ? 10 : 0}px;">${key}</h3>`;
+						let header = `<h3>${key}</h3>`;
 						let arr = 課文[this.option][key], detail = "";
 						for(let i = 0; i < arr.length; i++) {
 							// console.log(arr[i])
@@ -85,20 +111,114 @@ Vue.component('lesson', {
 									${arr[i].中文}
 								</li>`
 						}
-						this.html += `<ol style="margin: 0px 0px 0px 30px; ">${detail}</ol>`
+						this.html += `
+							<div class="section">
+								${header}
+								<ol style="margin: 0px 0px 0px 30px; ">${detail}</ol>
+							</div>`
 					}
-				}		
+					setTimeout(() => {
+						this.changeWidth();
+					}, 600);
+				}
 			}
-			this.scrollTop = 0;
-			window.localStorage["japanese-大家的日本語-lesson"] = this.option;
-			this.html = "";
+			let parseWord = () => {
+				if(typeof 單字[this.option] == "object") {
+					let arr = 單字[this.option];
+					for(let i = 0; i < arr.length; i++) {
+						let td = arr[i].split("\t");
+						let div1 = `
+							<div> <span style="width: 40px">${i + 1}.</span>
+								<a href="javascript: TTX.speak('${td[0]}');">${td[0]}</a>
+							</div>
+						`;
+						let div2 = `<div style="min-height: 24px;">${td[1]}</div>`;
+						let div3 = `<div>${td[2]}</div>`;
+						this.html += `<div class="card">${div1}${div2}${div3}</div>`
+					}
+					setTimeout(() => {
+						this.changeWidth();
+					}, 600);
+				}
+			}
+
 			setTimeout(() => {
-				parseLesson();
+				if(this.mode == "課文") 
+					parseLesson();
+				else {
+					parseWord();
+				}
+				setTimeout(() => {
+					this.changeWidth();
+				}, (this.mode == "課文") ? 600 : 1000);
 			}, 600);
     },
-		onChangeMode() {
+		changeWidth() {
+			let frame = this.$refs["frame"];
+			let arr = frame.querySelectorAll("div");
+			let width = "auto", marginLeft = "0px";
+			if(this.mode == "課文") {
+				if(frame.clientWidth > 1000) {
+					let w = Math.floor((frame.clientWidth - 60) / (arr.length >= 3 ? 3 : arr.length));
+					width = w + "px";
+					marginLeft = "20px";
+				}
+				for(let i = 0; i < arr.length; i++) {
+					arr[i].style.width = width;
+					arr[i].style.marginLeft = i == 0 ? "0px" : marginLeft;
+				}
+			} else {
+				let w1 = frame.clientWidth - 20;
+				if(frame.clientWidth > 800) {
+					let colNum = 0, margin = 10;
+					for(let i = 8; i >= 1; i--) {
+						let w = Math.floor((w1 - ((i -1) * margin)) / i);
+						if(w > 300) {
+							width = w + "px";
+							colNum = i;
+							break;
+						}
+					}
+				} else {
 
-		}
+				}
+
+				for(let i = 0; i < arr.length; i++) {
+					arr[i].style.width = width;
+					// arr[i].style.marginLeft = colNum == 0 ? "0px" : `${10}px`;
+				}
+			}
+			// setTimeout(() => {
+				if(arr.length)
+				 frame.style.visibility = "visible";	
+			// }, 600);
+		},
+		async onChangeMode() {
+			window.localStorage["japanese-大家的日本語-mode"] = this.mode;
+			this.options = [];
+			if(this.mode == "課文") {
+				await this.$appendScript("./datas/大家的日本語/課文.js");
+				for(let key in 課文){
+					this.options.push(key)
+				}
+				單字 = undefined;
+			} else {
+				await this.$appendScript("./datas/大家的日本語/單字.js");
+				for(let key in 單字){
+					this.options.push(key)
+				}
+				課文 = undefined;
+			}
+			if(typeof this.option == "undefined") this.option = "";
+
+			if(this.options.length == 0)
+				this.option = "";
+			else if(this.option.length == 0 && this.options.length > 0)
+				this.option = this.options[0];
+			
+			if(this.option.length > 0)
+				this.onChangeLesson();
+		} 
 	},
 	watch: {
 	},
