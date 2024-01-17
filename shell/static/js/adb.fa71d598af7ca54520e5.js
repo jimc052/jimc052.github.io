f1cebbe12a8b0520dfe420ca9e80a713a3974467
jim <jimc@bethel.com.tw>
2024-01-17 08:49:46 +0800

    2024-01-17

--- shell/static/js/adb.fa71d598af7ca54520e5.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{RyPl:function(e,t){},twTI:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o=n("7+uW"),l={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{attrs:{id:"adb"}},[this._v("\n  adb.vue test\n")])},staticRenderFns:[]};var s=n("VU/8")({name:"ADB",components:{}},l,!1,function(e){n("RyPl")},null,null).exports;o.default.config.productionTip=!1,console.log(s),new o.default({el:"#adb",components:{ADB:s},template:"<ADB />"})}},["twTI"]);
-//# sourceMappingURL=adb.fa71d598af7ca54520e5.js.map
--- shell/static/js/shell.d0847557dbf2c48c72c7.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{d5zm:function(e,t){},s7kq:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=n("7+uW"),s={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{attrs:{id:"shell"}},[this._v("\n  shell.vue test\n")])},staticRenderFns:[]};var i=n("VU/8")({name:"Shell",components:{}},s,!1,function(e){n("d5zm")},null,null).exports;l.default.config.productionTip=!1,new l.default({el:"#shell",components:{Shell:i},template:"<Shell />"})}},["s7kq"]);
-//# sourceMappingURL=shell.d0847557dbf2c48c72c7.js.map
