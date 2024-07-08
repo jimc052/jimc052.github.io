f33238ce5e1499dbce545654edfaf3090d6ea41c
jim <jimc@bethel.com.tw>
2024-07-08 09:05:33 +0800

    2024-07-08

--- bmi/list.js
+++ bmi/list.js
@@ -5,7 +5,16 @@ Vue.component('list', {
 		<div id="section">
 			<div class="row" v-for="(item, index) in datas"  @click="onClickRow(item, index);">
 				<div>{{item.date}}</div>
-				<div>{{item.weight}}</div>
+				<div style="display: flex; flex-direction: row; align-items: center;  justify-content: center;">
+					<span style="margin-right: 5px">{{item.weight}}</span>
+
+
+					<div style="width: 50px; font-size: 16px;">
+						<Icon v-if="item.compare > 0" type="md-arrow-up" size="14" style="color: red" />
+						<Icon v-if="item.compare < 0" type="md-arrow-down" size="14" style="color: blue" />
+					{{typeof item.compare == 'undefined' || item.compare == 0 ? '' : Math.abs(item.compare)}}</div>
+
+					</div>
 				<div :style="{color: item.bmi >= '18.00' && item.bmi <= '24.00' ? 'blue' : 'red' }">BMI：{{item.bmi}}</div>
 			</div>
 		</div>
@@ -16,7 +25,7 @@ Vue.component('list', {
 		/>
 		<dlg-height :visible="modalH" :height="height" @onClose="modalH = false" @onSave="onSaveHeight" />
 		<dlg-weight ref="weight" :visible="modalW" :height="height" @onClose="modalW = false" @onSave="onSaveWeight" />
-		<div id="footer">2024-07-05</div>
+		<div id="footer">2024-07-08</div>
 	</div>`,
 	props: {
 	},
@@ -40,6 +49,10 @@ Vue.component('list', {
 			if(this.datas[0].date.indexOf(s) == 0) { // 已新增過
 				this.today = s;
 			}
+			this.datas.forEach((el, index) => {
+				if(index < this.datas.length - 1)
+					this.compare(index);
+			});
 		}
 		// console.log(this.datas)
 
@@ -72,12 +85,24 @@ Vue.component('list', {
 		},
 		onSaveWeight(w) {
 			this.modalW = false;
-
-			this.datas.unshift(Object.assign({
+			let json = Object.assign({
 				date: (new Date()).toString("yyyy-mm-dd"), 
-			}, w))
-			// console.log(JSON.stringify(this.datas));
-			this.$storage("BMI", this.datas)
+			}, w);
+
+			let arr = this.$storage("BMI");
+			arr.unshift(json);
+			this.$storage("BMI", arr);
+
+			console.log(JSON.stringify(arr, null, 2));
+
+			this.datas.unshift(json);
+			if(this.datas.length >= 2)
+				this.compare(0);
+		},
+		compare(index) {
+			let w1 = parseFloat(this.datas[index].weight);
+			let w2 = parseFloat(this.datas[index + 1].weight);
+			this.datas[index].compare = (w1 - w2).toFixed(1);
 		}
 	},
 	watch: {
