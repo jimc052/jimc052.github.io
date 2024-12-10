b3099b497fce967917c7dafe14fd6325d7ed7aaa
jim <jimc@bethel.com.tw>
2024-12-10 09:13:19 +0800

    2024-12-10

---
+++ japanese/datas/youtube/playlist.js
@@ -0,0 +1,15 @@
+[
+  {
+    title: "快速學習 100 個日文形容詞",
+    url: "https://www.youtube.com/watch?v=zHlzLpOHkFU",
+  }, {
+    title: "初級日文動詞100個掌握",
+    url: "https://www.youtube.com/watch?v=smeDBe_rryQ",
+  }, {
+    title: "",
+    url: "", 
+  }, {
+    title: "",
+    url: "", 
+  }
+]
