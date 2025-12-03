4f63d08d26f1a2f9d0c86c7f900c0a991670c72d
jim <jimc@bethel.com.tw>
2025-12-03 13:32:09 +0800

    2025-12-03 13:30

--- japanese/letter-exam.js
+++ japanese/letter-exam.js
@@ -145,7 +145,7 @@ Vue.component('letter-exam', {
 					type="primary" size="large"  @click="similar" style="width: 100px; margin-top: 30px;">相似字</Button>
 			</div>
 			<div style="flex: 1" />
-			<div style="color: #2d8cf0; font-size: 24px;">2025-12-03 09:30</div>
+			<div style="color: #2d8cf0; font-size: 24px;">2025-12-03 13:30</div>
 			<!--
 			<div style="color: #2d8cf0; font-size: 24px;">{{"clientWidth " + this.clientWidth}}</div>
 			-->
